e76e454f805ba6a269b1a0585905a417e1f19237
Alejandra Bastidas <bastidasaleja@hotmail.com>
2021-11-09 22:25:47 -0500

    url login

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
           </Route>
           <Route path={['/']}>
             <LayoutInicial>
-              <Route path='/login'>
+              <Route path='/'>
                 <Login />
               </Route>
             </LayoutInicial>
